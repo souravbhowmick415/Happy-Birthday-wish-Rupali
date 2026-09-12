fc4d7109a93c363a29852aa7baa2ae50725dd0cd
souravbhowmick415 <souravbhowmick415@gmail.com>
2026-09-12 23:29:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,11 @@
 const photos = [
     "images/photo1.jpg",
     "images/photo2.jpg",
-    "images/photo3.jpg"
+    "images/photo3.jpg",
+    "images/photo4.jpg",
+    "images/photo5.jpg",
+    "images/photo6.jpg",
+    "images/photo7.jpg"
 ];
 
 let current = 0;
